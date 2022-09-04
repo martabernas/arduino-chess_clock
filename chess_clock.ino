4428ce16b3379f4ca7255e2a7396136752a1d013
Marta Bernas <58052208+martabernas@users.noreply.github.com>
2022-09-04 12:33:32 +0200

    Add files via upload

--- chess_clock.ino
+++ chess_clock.ino
@@ -4,12 +4,12 @@ LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 #define blackButton 11
 #define setButton 9
 
-int time1[2] = {10, 0};
-int time2[2] = {10, 0};
-int timeModes[5] = {1, 3, 5, 10, 15};
-unsigned long startTime = 0;
-int mode = 3;
-int counts = 0;
+int Time1[2] = {10, 0};
+int Time2[2] = {10, 0};
+int TimeModes[5] = {1, 3, 5, 10, 15};
+unsigned long StartTime = 0;
+int Mode = 3;
+int Counts = 0;
 
 
 void setup() {
@@ -23,10 +23,10 @@ void setup() {
 
 void loop() {
   if (digitalRead(blackButton) == LOW){
-    startTime = millis();
+    StartTime = millis();
     while(digitalRead(whiteButton) == HIGH){
-      if ((millis() - startTime) >= 1000){
-        startTime = millis();
+      if ((millis() - StartTime) >= 1000){
+        StartTime = millis();
         updateTime(1);
       }
       if (digitalRead(setButton) == LOW){
@@ -37,10 +37,10 @@ void loop() {
     }
   }
   if (digitalRead(whiteButton) == LOW){
-    startTime = millis();
+    StartTime = millis();
     while(digitalRead(blackButton) == HIGH){
-      if ((millis() - startTime) >= 1000){
-        startTime = millis();
+      if ((millis() - StartTime) >= 1000){
+        StartTime = millis();
         updateTime(2);
       }
       if (digitalRead(setButton) == LOW){
@@ -53,86 +53,90 @@ void loop() {
 
   if(digitalRead(setButton) == LOW){
     delay(1000);
-    counts = counts + 1;
-    if (counts == 2){
+    Counts = Counts + 1;
+    if (Counts == 2){
       while(digitalRead(setButton) == LOW){}
       setTime();
-      counts = 0;
+      Counts = 0;
     }
   }
   if(digitalRead(setButton) == HIGH){
-    counts = 0;
+    Counts = 0;
   }
 }
 
-  
+/* functions */
+
 void  updateTime(int player){
+/* Updates time by 1 second for given player 1 or 2 */
   int minutes;
   int seconds;
 
-  if(player == 1){
-    minutes = time1[0];
-    seconds = time1[1];
+  if(player == 1){ // read time into local variables
+    minutes = Time1[0];
+    seconds = Time1[1];
   } else if(player == 2){
-    minutes = time2[0];
-    seconds = time2[1];   
+    minutes = Time2[0];
+    seconds = Time2[1];   
   } else{
     minutes = 0;
     seconds = 0;
   }
 
-  if (minutes != 0 || seconds != 0){
-    if (seconds == 0){
+  if (minutes != 0 || seconds != 0){ // if time is not over, update by 1 second
+    if (seconds == 0){ // change minute
       seconds = 60;
       minutes = minutes - 1;
     }
     
-    seconds = seconds - 1;
+    seconds = seconds - 1; // change seconds
     
-    if (player == 1){
-      time1[0] = minutes;
-      time1[1] = seconds;
+    if (player == 1){ // update global variables
+      Time1[0] = minutes;
+      Time1[1] = seconds;
     } else if (player == 2){
-      time2[0] = minutes;
-      time2[1] = seconds;
+      Time2[0] = minutes;
+      Time2[1] = seconds;
     } else{
-      time1[0] = 0;
-      time1[1] = 0;
-      time2[0] = 0;
-      time2[1] = 0;
+      Time1[0] = 0;
+      Time1[1] = 0;
+      Time2[0] = 0;
+      Time2[1] = 0;
     }
       
-    displayTime(player);
+    displayTime(player); 
   
-  } else{ 
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      if (player == 1){
-        lcd.print("Black wins!");
-      } else if (player == 2){
-        lcd.print("White wins!");
-      }
+  } else{ // if time is over, print the winner
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    if (player == 1){
+      lcd.print("Black wins!");
+    } else if (player == 2){
+      lcd.print("White wins!");
+    }
   } 
 }
  
 void resetClock(void){
-  time1[0] = timeModes[mode];
-  time1[1] = 0;
-  time2[0] = timeModes[mode];
-  time2[1] = 0;
+  /* Reset clock to chosen Mode start value */
+  Time1[0] = TimeModes[Mode];
+  Time1[1] = 0;
+  Time2[0] = TimeModes[Mode];
+  Time2[1] = 0;
   startDisplay();
 }
 
 void displayTime(int player){
+  /* Display actual Time for given player 1 or 2 */
   int minutes;
   int seconds;
 
   if (player == 1){
-    minutes = time1[0];
-    seconds = time1[1];    
+    minutes = Time1[0];
+    seconds = Time1[1];    
   } else if (player == 2){
-    minutes = time2[0];
-    seconds = time2[1];   
+    minutes = Time2[0];
+    seconds = Time2[1];   
   } else {
     minutes = 0;
     seconds = 0;
@@ -155,28 +159,29 @@ void displayTime(int player){
 }
 
 void setTime(void){
-  while(digitalRead(setButton) == HIGH){
+  /* Start choosing clock time mode and change the mode */
+  while(digitalRead(setButton) == HIGH){ // if mode not confirmed wait for change
     lcd.setCursor(8, 0); 
     lcd.blink();
 
-    if(digitalRead(whiteButton) == LOW){
+    if(digitalRead(whiteButton) == LOW){ // change the mode
       lcd.noBlink();
-      mode = mode + 1;
-      if (mode > 4){
-        mode = 0;}
-
-      time1[0] = timeModes[mode];
-      time2[0] = timeModes[mode];
+      Mode = Mode + 1;
+      if (Mode > 4){
+        Mode = 0;
+      }
+      Time1[0] = TimeModes[Mode];
+      Time2[0] = TimeModes[Mode];
       displayTime(1);
       displayTime(2);
     }
       delay(200);
   }
-  lcd.noBlink();
   delay(500);
 }
 
 void startDisplay(void){
+  /* Display basic view: White and Black clocks */
   lcd.setCursor(0, 0);
   lcd.print("White: ");
   lcd.setCursor(9, 0);
